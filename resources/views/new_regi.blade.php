209424dd124ecc44b0340831f76657c423754906
Chelladurai <chelladurai.mma@gmail.com>
2020-10-27 21:14:18 +0530

    view pages

--- resources/views/new_regi.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app2')
-@section('content')
-    <div class="app-main__outer">
-        <div class="app-main__inner">
-        	<h1>chella</h1>
-        </div>
-    </div>
-@endsection
